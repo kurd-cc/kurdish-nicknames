d7b5983de6a5a8fc29f556e63a64a7e8bca50280
Jagar <jagaryousef@my.uopeople.edu>
2021-10-10 12:44:40 +0300

    make test for more results

--- test.js
+++ test.js
@@ -2,25 +2,25 @@ import {KurdishNicknames} from './index.js'
 import assert from 'assert'
 
 
-describe('generate 10 nicknames', function() {
+describe('generate 100 nicknames', function() {
     describe('#generate()', function() {
         it('should generate 10 names', function(done) {
-            let generated_names = KurdishNicknames.generate("both", 15, 10)
-            if (generated_names.length === 10){
+            let generated_names = KurdishNicknames.generate("both", 15, 100)
+            if (generated_names.length === 100){
                 done()
             }else{
-                done("10 names were not generated")
+                done("100 names were not generated")
             }
         });
     });
 });
 
 
-describe('generate 10 female nicknames', function() {
+describe('generate 100 female nicknames', function() {
     describe('#generate()', function() {
         it('should generate 10 female names', function(done) {
-            let generated_names = KurdishNicknames.generate("female", 15, 10)
-            if (generated_names.length === 10){
+            let generated_names = KurdishNicknames.generate("female", 15, 100)
+            if (generated_names.length === 100){
                 let i
                 for (i = 0; i < generated_names.length; i++){
                     if (generated_names[i].gender === "male"){
@@ -31,20 +31,21 @@ describe('generate 10 female nicknames', function() {
 
                 done()
             }else{
-                done("10 names were not generated")
+                done("100 names were not generated")
             }
         });
     });
 });
 
 
-describe('generate 10 male nicknames', function() {
+describe('generate 100 male nicknames', function() {
     describe('#generate()', function() {
         it('should generate 10 male names', function(done) {
-            let generated_names = KurdishNicknames.generate("male", 15, 10)
-            if (generated_names.length === 10){
+            let generated_names = KurdishNicknames.generate("male", 15, 100)
+            if (generated_names.length === 100){
                 let i
                 for (i = 0; i < generated_names.length; i++){
+
                     if (generated_names[i].gender === "female"){
                         done("The gender of one of the generated names was female (not male or both)!")
                         return
@@ -53,7 +54,7 @@ describe('generate 10 male nicknames', function() {
 
                 done()
             }else{
-                done("10 names were not generated")
+                done("100 names were not generated")
             }
         });
     });
